8aaa129a3d6ceb4ec11a0896333d29d7045c05d6
Tinu_thampi <79778565+Tinu-thampi13@users.noreply.github.com>
2021-10-19 14:59:53 +0530

    Changed the format & add more comments to the code

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from PIL import Image
+from PIL import Image #PILLOW- Python library that adds support for opening, manipulating, and saving many different image file formats.
 
-def listToString(s): 
-    
+
+def listToString(s):
+
     # initialize an empty string
-    str1 = "" 
-    
-    # traverse in the string  
-    for ele in s: 
-        str1 += ele  
-    
-    # return string  
-    return str1 
+    str1 = ""
+
+    # traverse in the string
+    for ele in s:
+        str1 += ele
+
+    # return string
+    return str1
+
 
 def write_css(box_shadow):
     print('Writing css')
@@ -27,21 +29,22 @@ def write_css(box_shadow):
         height: 0;
         box-shadow:'''+box_shadow[:-1]+''';}'''
 
-    with open('style.css','w') as fh:
+    with open('style.css', 'w') as fh:
         fh.write(css)
 
-img = Image.open('./sign.jpg')
-pixels = img.load() 
+
+img = Image.open('./sign.jpg') # open()- To load an image from a file
+pixels = img.load()
 width, height = img.size
 
 
-box_shadow = []
-for y in range(0,height,2):      # this row
-    for x in range(0,width,2):  
-        print('.') 
+box_shadow = [] # List Initializatin
+for y in range(0, height, 2):      # this row
+    for x in range(0, width, 2):
+        print('.')
         r, g, b = pixels[x, y]
-        hexv = f"#{r:02x}{g:02x}{b:02x}"
-        box_shadow.append(f'\n{x}px  {y}px 4px 5px #{r:02x}{g:02x}{b:02x},')
+        hexv = f"#{r:02x}{g:02x}{b:02x}" # Hexcode parsing
+        box_shadow.append(f'\n{x}px  {y}px 4px 5px hexv,')
 
 
 box_shadow = listToString(box_shadow)
