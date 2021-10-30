2f75a5d9bef6b8eae7cbca15e51e80cd9a59ca3c
sharonpraju <47088116+sharonpraju@users.noreply.github.com>
2021-10-30 21:41:16 +0530

    Preloader and Remove Button Integreted
    Preloader and button for removing image integrated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,20 @@
 const dropArea = document.querySelector('.drag-area');
 const dragText = document.querySelector('.header');
 
-let button = dropArea.querySelector('.button');
 let input = dropArea.querySelector('input');
 
 let file;
 
-button.onclick = () => {
+function inputClick()
+{
   input.click();
-};
+}
 
 // when browse
 input.addEventListener('change', function () {
   file = this.files[0];
   dropArea.classList.add('active');
+  $("#remove-btn").show();
   displayFile();
 });
 
@@ -35,12 +36,15 @@ dropArea.addEventListener('dragleave', () => {
 // when file is dropped
 dropArea.addEventListener('drop', (event) => {
   event.preventDefault();
-  // console.log('File is dropped in drag area');
+  //console.log('File is dropped in drag area');
+
+  $("#remove-btn").show();
 
   file = event.dataTransfer.files[0]; // grab single file even of user selects multiple files
   // console.log(file);
   displayFile();
 });
+
 
 function displayFile() {
   let fileType = file.type;
@@ -51,14 +55,39 @@ function displayFile() {
   if (validExtensions.includes(fileType)) {
     // console.log('This is an image file');
     let fileReader = new FileReader();
-
     fileReader.onload = () => {
       let fileURL = fileReader.result;
       // console.log(fileURL);
       let imgTag = `<img src="${fileURL}" alt="">`;
       dropArea.innerHTML = imgTag;
+      //fileURL is the base64 format of the image
+      //sending fileURL as ajax request
+      var img=fileURL;
+      $.ajax({
+        type: "POST",
+        url: "#",
+        data: {id:img},
+        success: function() {
+          //image uploaded sucessfully
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            console.log(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+        }
+      });
     };
     fileReader.readAsDataURL(file);
+
+    //displaying loading spinner over image
+    $('#drag-area').oLoader({
+      backgroundColor: '#000000ef',
+      fadeInTime: 500,
+      fadeLevel: 0.8,
+      image: 'assets/img/preloader.gif',
+      style: 2,
+      imagePadding: 5,
+      imageBgColor: 'transparent'
+    });
+
   } else {
     alert('This is not an Image File');
     dropArea.classList.remove('active');
